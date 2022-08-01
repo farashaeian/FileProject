6706aae436ac75ca3cd3f7eddcc9c78426d603ca
zahra <zahra@zahra.zahra>
2022-08-01 15:05:28 +0430

    add validation in UploadFileSerializer

--- count/serializers.py
+++ count/serializers.py
@@ -2,7 +2,7 @@ from .models import Dict, File
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
-from zipfile import is_zipfile
+from zipfile import is_zipfile, ZipFile, Path
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
@@ -26,7 +26,16 @@ class UploadFileSerializer(serializers.ModelSerializer):
         fields = ['file']
 
     def validate(self, attrs):
-        if not is_zipfile(attrs['file'].name):
+        if not is_zipfile(attrs['file']):
+            raise serializers.ValidationError("Uploaded File Is Not Zipped!")
+        temporary_extraction = ZipFile(attrs['file'].name)
+        zip_file_items = temporary_extraction.namelist()
+        valid_item = True
+        for item in zip_file_items:
+            validation_condition = item is Path.is_dir() or item.endswith("*.txt")
+            if not validation_condition:
+                valid_item = False
+        if not valid_item:
             raise serializers.ValidationError("Uploaded File Is Not Zipped!")
         return attrs
 
