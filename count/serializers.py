74b0c2b11c019c7c24e5b4bc15eb8bec550d5904
zahra <zahra@zahra.zahra>
2022-08-09 15:00:08 +0430

    make functions in serializer for extracting zipped file and finding files and folders which in it

--- count/serializers.py
+++ count/serializers.py
@@ -28,12 +28,15 @@ class UploadFileSerializer(serializers.ModelSerializer):
         default=serializers.CurrentUserDefault(),
         queryset=User.objects.all()
     )
-    # define other file and category fields explicitly here and
-    # send them by validated_data
-
+    """
+    name = serializers.CharField()
+    father = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    path = serializers.CharField()
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    """
     class Meta:
         model = File
-        fields = ['file', 'user', 'category']
+        fields = ['file', 'user', 'category']   # , 'name', 'father', 'path'
         read_only_fields = ['category']
 
     def validate(self, attrs):
@@ -48,59 +51,71 @@ class UploadFileSerializer(serializers.ModelSerializer):
                 valid_item = False
         if not valid_item:
             raise serializers.ValidationError("Invalid Items!")
-        # !! move the extraction code to the proper place !!
-        with ZipFile(attrs['file'], mode='r', allowZip64=True) as extracted_file:
+        return attrs
+
+    def extract_zip_file(self):
+        with ZipFile(self['file'], mode='r', allowZip64=True) as extracted_file:
             current_user_id = self.context.get('request').user.id
             extract_path = 'Documents/uploaded_files/user_{0}/'.format(current_user_id)
             extracted_file.extractall(path=extract_path)
         extracted_file.close()
-        return attrs
 
-    def create(self, validated_data):
+    def find_folders(self):
         folder_list = []
-        file_list = []
-        file_list_obj = []
-        folder_list_obj = []
         current_user_id = self.context.get('request').user.id
         root = 'Documents/uploaded_files/user_{0}/{1}'.format(
             current_user_id,
-            validated_data['file']
+            self['file']
         )
         for root, dirs, files in os.walk(root):
             for d in dirs:
                 folder_list.append(os.path.join(root, d))
+        return folder_list
+
+    def find_files(self):
+        file_list = []
+        current_user_id = self.context.get('request').user.id
+        root = 'Documents/uploaded_files/user_{0}/{1}'.format(
+            current_user_id,
+            self['file']
+        )
+        for root, dirs, files in os.walk(root):
             for f in files:
                 file_list.append(os.path.join(root, f))
-        # return File(**validated_data)
+        return file_list
+
+    def create(self, validated_data):
+        folder_list_obj = []
+        file_list_obj = []
+        folder_list = self.find_folders()
+        file_list = self.find_files()
         for f in folder_list:
             # !!have to change the name saving for folders!!
             # !!save the single name of folder not the path instead of the name!!
             # !!find solution for name duplication and querying father id!!
             last_slash_index = f.rindex('/')
             folder_father_name = f[:last_slash_index]
-            folder_father_id = Category.objects.find(name=folder_father_name).id
+            folder_father = Category.objects.find(name=folder_father_name)
             folder_obj = Category(
                 name=f,
                 user=validated_data['user'],
-                father=folder_father_id
+                father=folder_father
             )
             folder_list_obj.append(folder_obj)
-        # Category.objects.bulk_create(folder_list_obj)
         folders = [Category(**item) for item in validated_data]
         Category.objects.bulk_create(folders)
         for f in file_list:
             last_slash_index = f.rindex('/')
             file_category_name = f[:last_slash_index]
-            file_category_id = Category.objects.find(name=file_category_name).id
+            file_category = Category.objects.find(name=file_category_name)
             file_obj = File(
                 path=f,
                 user=validated_data['user'],
-                category=file_category_id
+                category=file_category
             )
             file_list_obj.append(file_obj)
         files = [File(**item) for item in validated_data]
         return File.objects.bulk_create(files)
-        # return File.objects.bulk_create(file_list_obj)
 
 
 class ShowFolderSerializer(serializers.ModelSerializer):
