568cc55eb11c4a9b896940c324b667b080977be9
zahra <zahra@zahra.zahra>
2022-08-24 15:03:57 +0430

    add tests for registering user and listing dictionary

--- count/test/test_dictionary.py
+++ count/test/test_dictionary.py
@@ -4,21 +4,33 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from count.models import Dict
+from random import randint
 
 
 class DictTests(APITestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.url = 'my_dict'
+        cls.url = reverse('my_dict')
         cls.user = mommy.make(User, 3)
-        cls.dict = mommy.make(Dict, 3)
+        cls.dict = mommy.make(Dict, 20)
 
-        cls.dict[0].user.set(cls.user[0].id)
-        cls.dict[1].user.set(cls.user[1].id)
-        cls.dict[2].user.set(cls.user[2].id)
-        cls.client.force_login(cls.user[0])
+    def setUp(self):
+        self.random_id = randint(1, 3)
+        self.client.force_login(self.user[self.random_id])
 
     def test_list_dictionary_successfully(self):
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        current_user_dict = Dict.objects.filter(user=self.user[self.random_id]).order_by('-number')
+        self.assertEqual(len(response.data), current_user_dict.count())
+        for i in range(len(response.data)):
+            self.assertEqual(response.data[i]['word'], current_user_dict[i].word)
+            self.assertEqual(response.data[i]['user'], current_user_dict[i].user)
+            self.assertEqual(response.data[i]['number'], current_user_dict[i].number)
+        i, j = 0, 1
+        while i != len(response.data):
+            self.assertGreaterEqual(
+                response.data[i]['number'],
+                current_user_dict[i].number
+            )
