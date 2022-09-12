94f409e80f32b838945e63094c3ba1d5965ac889
zahra <zahra@zahra.zahra>
2022-09-12 15:03:46 +0430

    add comparison tests to the test_file_successfully.py

---
+++ count/custom_methods.py
@@ -0,0 +1,103 @@
+from .models import File, Category, Dict
+from zipfile import ZipFile
+import os
+from nltk.tokenize import word_tokenize
+from spellchecker import SpellChecker
+
+
+def extract_zip_file(zip_file_obj, user):
+    with ZipFile(zip_file_obj.file, mode='r', allowZip64=True) as extracted_file:
+        current_user_id = user.id
+        extract_path = 'Documents/uploaded_files/user_{0}/'.format(current_user_id)
+        extracted_file.extractall(path=extract_path)
+    extracted_file.close()
+
+
+def find_folders(root_folder_path):
+    folder_list = []
+    for root, dirs, files in os.walk(root_folder_path):
+        for d in dirs:
+            folder_list.append(os.path.join(root, d))
+    return folder_list
+
+
+def find_files(root_folder_path):
+    file_list = []
+    for root, dirs, files in os.walk(root_folder_path):
+        for f in files:
+            file_list.append(os.path.join(root, f))
+    return file_list
+
+
+def text_file_tokenize(file_path):
+    opened_file = open(file_path, 'r')
+    text = opened_file.read()
+    opened_file.close()
+    file_text = word_tokenize(text)
+    return file_text
+
+
+def analyze_text_file(file_path, user):
+    file_text = text_file_tokenize(file_path)
+    analyze = {'new': 0, "duplicate": 0, "typo": 0}
+    for word in file_text:
+        is_typo = SpellChecker().unknown([word])
+        try:
+            word_in_Dict = Dict.objects.get(word=word, user=user)
+        except Dict.DoesNotExist:
+            word_in_Dict = None
+        if is_typo:
+            # increase typo number in the file
+            analyze['typo'] += 1
+        elif word_in_Dict:
+            # increase word number in the user dictionary
+            new_word_number = word_in_Dict.number + 1
+            word_in_Dict.number = new_word_number
+            word_in_Dict.save()
+            # increase word duplication in the file
+            analyze['duplicate'] += 1
+        else:
+            # add new word to the user Dictionary
+            new_word = Dict(
+                word=word,
+                number=1,
+                user=user
+            )
+            new_word.save()
+            # increase new word number in the file
+            analyze['new'] += 1
+    return analyze
+
+
+def save_folders(folder_list, user):
+    for f in folder_list:
+        last_slash_index = f.rindex('/')
+        folder_father_path = f[:last_slash_index]
+        folder_father_obj = Category.objects.get(path=folder_father_path)
+        folder_name = f[(last_slash_index + 1):]
+        folder_obj = Category(
+            path=f,
+            display_name=folder_name,
+            user=user,
+            father=folder_father_obj
+        )
+        folder_obj.save()
+
+
+def save_files(file_list, user):
+    for f in file_list:
+        last_slash_index = f.rindex('/')
+        file_category_path = f[:last_slash_index]
+        file_category = Category.objects.get(path=file_category_path)
+        file_name = f[(last_slash_index + 1):]
+        analyze = analyze_text_file(f, user)
+        file_obj = File(
+            path=f,
+            display_name=file_name,
+            user=user,
+            category=file_category,
+            new=analyze['new'],
+            duplicate=analyze['duplicate'],
+            typo=analyze['typo']
+        )
+        file_obj.save()
--- count/test/test_file_successfully.py
+++ count/test/test_file_successfully.py
@@ -7,7 +7,7 @@ from count.models import File, Category
 import os
 from count.tasks import unzip
 from zipfile import ZipFile
-
+from count.custom_methods import analyze_text_file
 
 class FileTestsSuccessfully(APITestCase):
     @classmethod
@@ -19,7 +19,7 @@ class FileTestsSuccessfully(APITestCase):
         self.client.force_login(self.user)
 
     def test_celery_upload_file_successfully_save_zipfile_in_db_successfully(self):
-        file_from_system = "count/test/sample_zip_files/sample8.zip"
+        file_from_system = "count/test/sample_zip_files/sample9.zip"
 
         with open(file_from_system, 'rb') as fp:
             response = self.client.post(self.url, {'file': fp})
@@ -44,7 +44,7 @@ class FileTestsSuccessfully(APITestCase):
         self.assertEqual(db_zip_file.typo, 0)
 
     def test_celery_upload_file_successfully_save_zipfile_in_expected_path_successfully(self):
-        file_from_system = "count/test/sample_zip_files/sample5.zip"
+        file_from_system = "count/test/sample_zip_files/sample8.zip"
 
         with open(file_from_system, 'rb') as fp:
             response = self.client.post(self.url, {'file': fp})
@@ -72,7 +72,7 @@ class FileTestsSuccessfully(APITestCase):
         self.assertEqual(current_zipfile_number_in_path, 1)
 
     def test_celery_upload_file_successfully_save_root_folder_in_db_successfully(self):
-        file_from_system = "count/test/sample_zip_files/sample4.zip"
+        file_from_system = "count/test/sample_zip_files/sample7.zip"
 
         with open(file_from_system, 'rb') as fp:
             response = self.client.post(self.url, {'file': fp})
@@ -99,7 +99,7 @@ class FileTestsSuccessfully(APITestCase):
         self.assertEqual(db_extracted_folder.user_id, self.user.id)
 
     def test_celery_upload_file_successfully_save_root_folder_in_expected_path_successfully(self):
-        file_from_system = "count/test/sample_zip_files/sample3.zip"
+        file_from_system = "count/test/sample_zip_files/sample6.zip"
 
         with open(file_from_system, 'rb') as fp:
             response = self.client.post(self.url, {'file': fp})
@@ -132,8 +132,52 @@ class FileTestsSuccessfully(APITestCase):
                 current_folder_number_in_path += 1
         self.assertEqual(current_folder_number_in_path, 1)
 
-    def test_celery_upload_file_successfully_correct_extraction(self):
-        file_from_system = "count/test/sample_zip_files/sample7.zip"
+    def test_celery_upload_file_successfully_comparison_selected_zipfile_with_uploaded_zipfile_successfully(self):
+        file_from_system = "count/test/sample_zip_files/sample5.zip"
+
+        with open(file_from_system, 'rb') as zf:
+            response = self.client.post(self.url, {'file': zf})
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['message'], 'The File Was Received.')
+
+        # compare selected zipfile with uploaded zipfile content(item numbers up to their types and their path):
+
+        # selected zipfile content:
+        selected_zip_file_items_list = ZipFile(file_from_system).namelist()
+        s_file_list_number, s_dir_list_number = 0, 0
+
+        for item in selected_zip_file_items_list:
+            if item.endswith('/'):
+                s_dir_list_number += 1
+            elif item.endswith('.txt'):
+                s_file_list_number += 1
+
+        # uploaded zipfile content:
+        expected_zip_file_name = file_from_system.split('/')[-1]
+        expected_zip_file_path = 'Documents/uploaded_files/user_{0}/{1}'.format(
+            self.user.id, expected_zip_file_name)
+
+        uploaded_zip_file_items_list = ZipFile(expected_zip_file_path).namelist()
+        u_file_list_number, u_dir_list_number = 0, 0
+
+        for item in uploaded_zip_file_items_list:
+            if item.endswith('/'):
+                u_dir_list_number += 1
+            elif item.endswith('.txt'):
+                u_file_list_number += 1
+
+        # asserts:
+        # compare items paths:
+        self.assertEqual(selected_zip_file_items_list, uploaded_zip_file_items_list)
+        # compare items number up to their types:
+        self.assertEqual(len(selected_zip_file_items_list), len(uploaded_zip_file_items_list))
+        self.assertEqual(s_dir_list_number, u_dir_list_number)
+        self.assertEqual(s_file_list_number, u_file_list_number)
+
+    def test_celery_upload_file_successfully_comparison_uploaded_zipfile_with_extracted_folder_successfully(self):
+        """correct extraction"""
+        file_from_system = "count/test/sample_zip_files/sample4.zip"
 
         with open(file_from_system, 'rb') as fp:
             response = self.client.post(self.url, {'file': fp})
@@ -147,43 +191,117 @@ class FileTestsSuccessfully(APITestCase):
         unzip_task_result = unzip.apply(args=(db_zip_file.path, self.user.id)).get()
         self.assertEqual(unzip_task_result['message'], 'successful Process')
 
-        # check the  zip file's content (text files and dirs) &
-        # extracted folder content equality (number & path):
-        zip_file_items_list = ZipFile(file_from_system).namelist()
-        zip_file_folders_list = []
-        zip_file_folders_number = 0
-        zip_file_text_files_list = []
-        zip_file_text_files_number = 0
-        for item in zip_file_items_list:
+        # uploaded zipfile:
+        expected_zip_file_path = 'Documents/uploaded_files/user_{0}/{1}'.format(
+            self.user.id, expected_zip_file_name)
+
+        uploaded_zip_file_items_list = ZipFile(expected_zip_file_path).namelist()
+        # delete root folder from the list; because root folder doesn't contain itself
+        expected_extracted_folder_name = expected_zip_file_name.split('.')[0]
+        root_folder_path = '{0}/'.format(expected_extracted_folder_name)
+        uploaded_zip_file_items_list.remove(root_folder_path)
+
+        u_file_list_number, u_dir_list_number = 0, 0
+
+        for item in uploaded_zip_file_items_list:
             if item.endswith('/'):
-                zip_file_folders_list.append(item)
-                zip_file_folders_number += 1
-            elif item.endswith(".txt"):
-                zip_file_text_files_list.append(item)
-                zip_file_text_files_number += 1
+                u_dir_list_number += 1
+            elif item.endswith('.txt'):
+                u_file_list_number += 1
 
-        extracted_folder_name = expected_zip_file_name.split('.')[0]
-        extracted_folder_path = 'Documents/uploaded_files/user_{0}/{1}'.format(
-            self.user.id, extracted_folder_name)
-        extracted_folder_dir_list = []
-        extracted_folder_dir_number = 0
-        extracted_folder_text_file_list = []
-        extracted_folder_text_file_number = 0
-        for root, dirs, files in os.walk(extracted_folder_path):
+        # extracted folder:
+        expected_extracted_folder_path = 'Documents/uploaded_files/user_{0}/{1}'.format(
+            self.user.id, expected_extracted_folder_name)
+
+        e_file_list, e_dir_list = [], []
+        e_file_list_number, e_dir_list_number = 0, 0
+        user_document_path = 'Documents/uploaded_files/user_{0}/'.format(self.user.id)
+
+        for root, dirs, files in os.walk(expected_extracted_folder_path):
             for d in dirs:
-                extracted_folder_dir_list.append(os.path.join(root, d))
-                extracted_folder_dir_number += 1
+                e_dir_list_number += 1
+                temp_dir = ((os.path.join(root, d)).replace(user_document_path, ''))+'/'
+                e_dir_list.append(temp_dir)
             for f in files:
-                extracted_folder_text_file_list.append(os.path.join(root, f))
-                extracted_folder_text_file_number += 1
+                e_file_list_number += 1
+                temp_file = (os.path.join(root, f)).replace(user_document_path, '')
+                e_file_list.append(temp_file)
+        extracted_folder_items_list = e_dir_list + e_file_list
 
-        self.assertEqual(zip_file_folders_number-1, extracted_folder_dir_number)
-        self.assertEqual(zip_file_text_files_number, extracted_folder_text_file_number)
-        # check folders paths : paths need to be in same format
-        # check text files paths : paths need to be in same format
+        # asserts:
+        # compare items paths:
+        # Note: use assertIn instead of assertEqual because lists orders are not same.
+        for item in uploaded_zip_file_items_list:
+            self.assertIn(
+                item,
+                extracted_folder_items_list
+            )
+        # compare items number up to their types:
+        self.assertEqual(
+            len(uploaded_zip_file_items_list),
+            e_dir_list_number + e_file_list_number
+        )
+        self.assertEqual(u_dir_list_number, e_dir_list_number)
+        self.assertEqual(u_file_list_number, e_file_list_number)
 
-    def test_celery_upload_file_successfully_correct_content_saving_in_db(self):
-        pass
-        # sample1
-        # uploaded zip file's content (text files and folders) be equal by text files
-        # and folders in DB (number and content)
+    def test_celery_upload_file_successfully_comparison_extracted_folder_with_db_successfully(self):
+        file_from_system = "count/test/sample_zip_files/sample1.zip"
+
+        with open(file_from_system, 'rb') as zf:
+            response = self.client.post(self.url, {'file': zf})
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['message'], 'The File Was Received.')
+
+        # call the celery.task (unzip):
+        expected_zip_file_name = file_from_system.split('/')[-1]
+        db_zip_file = File.objects.get(category=None, display_name=expected_zip_file_name)
+        unzip_task_result = unzip.apply(args=(db_zip_file.path, self.user.id)).get()
+        self.assertEqual(unzip_task_result['message'], 'successful Process')
+
+        # extracted folder:
+        expected_extracted_folder_name = expected_zip_file_name.split('.')[0]
+        expected_extracted_folder_path = 'Documents/uploaded_files/user_{0}/{1}'.format(
+            self.user.id, expected_extracted_folder_name)
+
+        e_file_list, e_dir_list = [], []
+        e_file_list_number, e_dir_list_number = 0, 0
+
+        for root, dirs, files in os.walk(expected_extracted_folder_path):
+            for d in dirs:
+                e_dir_list_number += 1
+                e_dir_list.append(os.path.join(root, d))
+            for f in files:
+                e_file_list_number += 1
+                e_file_list.append(os.path.join(root, f))
+
+        # check extracted folder dirs exist in the DB:
+        for item in e_dir_list:
+            db_item = Category.objects.get(path=item)
+
+            item_display_name = item.split('/')[-1]
+            x = '/{0}'.format(item_display_name)
+            item_father_path = item.replace(x, "")
+            db_item_father = Category.objects.get(path=item_father_path)
+
+            self.assertEqual(db_item.display_name, item_display_name)
+            self.assertEqual(db_item.user_id, self.user.id)
+            self.assertEqual(db_item.father_id, db_item_father.id)
+
+        # check extracted folder text files exist in the DB:
+        for item in e_file_list:
+            db_item = File.objects.get(path=item)
+
+            item_display_name = item.split('/')[-1]
+            x = '/{0}'.format(item_display_name)
+            item_category_path = item.replace(x, "")
+            db_item_category = Category.objects.get(path=item_category_path)
+            item_analyze = analyze_text_file(item, self.user)
+
+            # self.assertEqual(db_item.file, item_file)
+            self.assertEqual(db_item.display_name, item_display_name)
+            self.assertEqual(db_item.user_id, self.user.id)
+            self.assertEqual(db_item.category_id, db_item_category.id)
+            # self.assertEqual(db_item.new, item_analyze['new'])
+            # self.assertEqual(db_item.duplicate, item_analyze['duplicate'])
+            # self.assertEqual(db_item.typo, item_analyze['typo'])
